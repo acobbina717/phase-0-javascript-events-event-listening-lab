cbba51700aca49ef12421ed5e07372241f0cf8b3
Abeku <acobbina717@gmail.com>
2022-04-08 17:00:12 -0400

    commit tree 956a1c51f9424dfb733a83f8ea7f6ff7e9533c96

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 function addingEventListener() {
+  const input = document.getElementById("input")
+  const clickAlert = () => alert("I was clicked!")
+  input.addEventListener('click', clickAlert);
 }
